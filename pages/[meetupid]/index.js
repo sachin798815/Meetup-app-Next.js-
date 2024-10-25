9c6d962baf12e0bf687861593d28332d5c9a07de
sachin798815 <sachin798815@gmail.com>
2024-10-25 23:33:05 +0530

    show details fixed

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,47 +1,64 @@
+import { MongoClient, ObjectId } from "mongodb";
+
 function MeetupDetails({ meetupData }) {
-    return (
-      <>
-        <p>
-          <img
-            src={meetupData.image}
-            alt={meetupData.title}
-            style={{ width: "100px" }}
-          />
-          <h1>{meetupData.title}</h1>
-          <address>{meetupData.adress}</address>
-          <p>{meetupData.description}</p>
-        </p>
-      </>
-    );
-  }
-  
-  export async function getStaticPaths() {
-    return {
-      fallback: false,
-      paths: [
-        { params: { meetupid: 'm1' } },
-        { params: { meetupid: 'm2' } },
-        { params: { meetupid: 'm3' } },
-      ],
-    };
-  }
-  
-  export async function getStaticProps(context) {
-    const meetupId = context.params.meetupid;
-  
-    return {
-      props: {
-        meetupData: {
-          image: "https://png.pngtree.com/background/20230412/original/pngtree-architectural-modern-high-rise-buildings-background-picture-image_2399417.jpg",
-          id: meetupId,
-          title: "First meetup",
-          adress: "Hno. 4",
-          description: "hey there, welcome",
-        },
+  if (!meetupData) return <p>Meetup not found!</p>;
+
+  return (
+    <>
+      <img
+        src={meetupData.image}
+        alt={meetupData.title}
+        style={{ width: "100px" }}
+      />
+      <h1>{meetupData.title}</h1>
+      <address>{meetupData.address}</address>
+      <p>{meetupData.description}</p>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://test:test@cluster0.qnlqt.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupid: meetup._id.toString(),
       },
-      revalidate: 10,
-    };
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupid;
+  const client = await MongoClient.connect(
+    "mongodb+srv://test:test@cluster0.qnlqt.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  // Convert meetupId to ObjectId for MongoDB query
+  const selectedMeetup = await meetupsCollection.findOne({ _id: new ObjectId(meetupId) });
+  client.close();
+
+  // Convert the _id field to a string to make it serializable
+  if (selectedMeetup) {
+    selectedMeetup._id = selectedMeetup._id.toString();
   }
-  
-  export default MeetupDetails;
-  
+
+  return {
+    props: {
+      meetupData: selectedMeetup || {}, // Adding fallback for empty data
+    },
+  };
+}
+
+export default MeetupDetails;
