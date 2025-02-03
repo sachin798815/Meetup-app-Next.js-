1f439a70e15ba2b9165113137e76a75165450cfa
sachin798815 <sachin798815@gmail.com>
2025-02-03 12:12:38 +0530

    improved readability in form component

--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -1,21 +1,15 @@
-import { useRef } from 'react';
-
+import { useState } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm(props) {
-  const titleInputRef = useRef();
-  const imageInputRef = useRef();
-  const addressInputRef = useRef();
-  const descriptionInputRef = useRef();
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredImage, setEnteredImage] = useState('');
+  const [enteredAddress, setEnteredAddress] = useState('');
+  const [enteredDescription, setEnteredDescription] = useState('');
 
   function submitHandler(event) {
     event.preventDefault();
-
-    const enteredTitle = titleInputRef.current.value;
-    const enteredImage = imageInputRef.current.value;
-    const enteredAddress = addressInputRef.current.value;
-    const enteredDescription = descriptionInputRef.current.value;
 
     const meetupData = {
       title: enteredTitle,
@@ -25,34 +19,54 @@ function NewMeetupForm(props) {
     };
 
     props.onAddMeetup(meetupData);
+
   }
 
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' ref={titleInputRef} />
+          <label htmlFor="title">Meetup Title</label>
+          <input 
+            type="text" 
+            required 
+            id="title" 
+            value={enteredTitle} 
+            onChange={(event) => setEnteredTitle(event.target.value)} 
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' ref={imageInputRef} />
+          <label htmlFor="image">Meetup Image</label>
+          <input 
+            type="url" 
+            required 
+            id="image" 
+            value={enteredImage} 
+            onChange={(event) => setEnteredImage(event.target.value)} 
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' ref={addressInputRef} />
+          <label htmlFor="address">Address</label>
+          <input 
+            type="text" 
+            required 
+            id="address" 
+            value={enteredAddress} 
+            onChange={(event) => setEnteredAddress(event.target.value)} 
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor='description'>Description</label>
+          <label htmlFor="description">Description</label>
           <textarea
-            id='description'
+            id="description"
             required
-            rows='5'
-            ref={descriptionInputRef}
+            rows="5"
+            value={enteredDescription}
+            onChange={(event) => setEnteredDescription(event.target.value)}
           ></textarea>
         </div>
         <div className={classes.actions}>
-          <button>Add Meetup</button>
+          <button type="submit">Add Meetup</button>
         </div>
       </form>
     </Card>
