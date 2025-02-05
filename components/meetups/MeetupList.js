fed16658acaf9bd9df52ea7db90e2e626c4a82e4
sachin798815 <sachin798815@gmail.com>
2025-02-05 12:35:26 +0530

    meetup list empty checker added

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,16 +1,20 @@
 import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
-function MeetupList(props) {
+function MeetupList({ meetups }) {
+  if (!meetups || meetups.length === 0) {
+    return <p className={classes.empty}>No meetups available.</p>;
+  }
+
   return (
     <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
+      {meetups.map(({ id, image, title, address }) => (
         <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
+          key={id}
+          id={id}
+          image={image}
+          title={title}
+          address={address}
         />
       ))}
     </ul>
