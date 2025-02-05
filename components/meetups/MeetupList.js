6f15e0666678d15493ee57fb75ffc072382dcd13
sachin798815 <sachin798815@gmail.com>
2025-02-05 12:36:10 +0530

    meetup list user friendly messages added

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -3,21 +3,23 @@ import classes from './MeetupList.module.css';
 
 function MeetupList({ meetups }) {
   if (!meetups || meetups.length === 0) {
-    return <p className={classes.empty}>No meetups available.</p>;
+    return <p className={classes.empty}>No meetups available. Please check back later!</p>;
   }
 
   return (
-    <ul className={classes.list}>
-      {meetups.map(({ id, image, title, address }) => (
-        <MeetupItem
-          key={id}
-          id={id}
-          image={image}
-          title={title}
-          address={address}
-        />
-      ))}
-    </ul>
+    <section className={classes.container}>
+      <ul className={classes.list}>
+        {meetups.map(({ id, image, title, address }) => (
+          <MeetupItem
+            key={id}
+            id={id}
+            image={image}
+            title={title}
+            address={address}
+          />
+        ))}
+      </ul>
+    </section>
   );
 }
 
