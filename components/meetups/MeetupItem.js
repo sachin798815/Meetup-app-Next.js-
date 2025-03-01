6e5e23eb25c456871ed13fe66e048852fa50c317
sachin798815 <sachin798815@gmail.com>
2025-03-01 12:45:58 +0530

    meetup item image handling

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,27 +1,33 @@
 import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+import Image from 'next/image';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem({ id, image, title, address }) {
   const router = useRouter();
 
-  const showDetailsHandler = () => {
+  const showDetailsHandler = useCallback(() => {
     router.push(`/${id}`);
-  };
+  }, [id, router]);
 
   return (
     <li className={classes.item}>
       <Card>
-        <div className={classes.image}>
-          <img src={image} alt={title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{title}</h3>
-          <address>{address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={showDetailsHandler}>Show Details</button>
-        </div>
+        <article className={classes.article}>
+          <div className={classes.image}>
+            <Image src={image} alt={title} width={400} height={250} layout="responsive" />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <address>{address}</address>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={showDetailsHandler} className={classes.button}>
+              Show Details
+            </button>
+          </div>
+        </article>
       </Card>
     </li>
   );
