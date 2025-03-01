d9cb627725f7880302745001eeaac672662e3ff9
sachin798815 <sachin798815@gmail.com>
2025-03-01 12:44:17 +0530

    meetup item props extracted

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -2,20 +2,22 @@ import { useRouter } from 'next/router';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
-function MeetupItem(props) {
-  const router=useRouter();
-  const showDetailsHandler=()=>{
-    router.push('/'+props.id)
-  }
+function MeetupItem({ id, image, title, address }) {
+  const router = useRouter();
+
+  const showDetailsHandler = () => {
+    router.push(`/${id}`);
+  };
+
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <img src={image} alt={title} />
         </div>
         <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
+          <h3>{title}</h3>
+          <address>{address}</address>
         </div>
         <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
