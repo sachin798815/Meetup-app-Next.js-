cd1adacb40dee82bf33db78252c5143c8a763131
sachin798815 <sachin798815@gmail.com>
2025-03-03 11:41:43 +0530

    meetup list new structure

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,24 +1,19 @@
 import MeetupItem from './MeetupItem';
 import classes from './MeetupList.module.css';
 
-function MeetupList({ meetups }) {
-  if (!meetups || meetups.length === 0) {
-    return <p className={classes.empty}>No meetups available. Please check back later!</p>;
-  }
-
+function MeetupList({ meetups = [] }) {
   return (
-    <section className={classes.container}>
-      <ul className={classes.list}>
-        {meetups.map(({ id, image, title, address }) => (
-          <MeetupItem
-            key={id}
-            id={id}
-            image={image}
-            title={title}
-            address={address}
-          />
-        ))}
-      </ul>
+    <section className={classes.container} aria-labelledby="meetup-heading">
+      <h2 id="meetup-heading" className={classes.heading}>Upcoming Meetups</h2>
+      {meetups.length === 0 ? (
+        <p className={classes.empty}>No meetups available. Please check back later!</p>
+      ) : (
+        <ul className={classes.list}>
+          {meetups.map((meetup) => (
+            <MeetupItem key={meetup.id} {...meetup} />
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
