12dadb8220c33af5c20b8a561c99ef158e06229b
sachin798815 <sachin798815@gmail.com>
2025-03-03 11:42:41 +0530

    list destructuring

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -6,11 +6,11 @@ function MeetupList({ meetups = [] }) {
     <section className={classes.container} aria-labelledby="meetup-heading">
       <h2 id="meetup-heading" className={classes.heading}>Upcoming Meetups</h2>
       {meetups.length === 0 ? (
-        <p className={classes.empty}>No meetups available. Please check back later!</p>
+        <p className={classes.empty}>No meetups available. Check back soon!</p>
       ) : (
         <ul className={classes.list}>
-          {meetups.map((meetup) => (
-            <MeetupItem key={meetup.id} {...meetup} />
+          {meetups.map(({ id, ...meetupProps }) => (
+            <MeetupItem key={id} id={id} {...meetupProps} />
           ))}
         </ul>
       )}
