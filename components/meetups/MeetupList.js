f77982abbd3fcbff0bc471421aae7a52e35a8d4c
sachin798815 <sachin798815@gmail.com>
2025-03-04 12:04:58 +0530

    List updated

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -6,11 +6,13 @@ function MeetupList({ meetups = [] }) {
     <section className={classes.container} aria-labelledby="meetup-heading">
       <h2 id="meetup-heading" className={classes.heading}>Upcoming Meetups</h2>
       {meetups.length === 0 ? (
-        <p className={classes.empty}>No meetups available. Check back soon!</p>
+        <p className={classes.empty}>No meetups available. Stay tuned for updates!</p>
       ) : (
         <ul className={classes.list}>
           {meetups.map(({ id, ...meetupProps }) => (
-            <MeetupItem key={id} id={id} {...meetupProps} />
+            <li key={id} className={classes.item}>
+              <MeetupItem id={id} {...meetupProps} />
+            </li>
           ))}
         </ul>
       )}
