d36f14017d757a93f3499a54cd49a8d13adf20e4
sachin798815 <sachin798815@gmail.com>
2025-03-22 12:00:52 +0530

    nav active links

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,17 +1,24 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
+  const router = useRouter();
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link href="/">All Meetups</Link>
+            <Link href="/" className={router.pathname === '/' ? classes.active : ''}>
+              All Meetups
+            </Link>
           </li>
           <li>
-            <Link href="/new-meetup">Add New Meetup</Link>
+            <Link href="/new-meetup" className={router.pathname === '/new-meetup' ? classes.active : ''}>
+              Add New Meetup
+            </Link>
           </li>
         </ul>
       </nav>
