9c83356960dddfad8940b6909a38aba8eaa6a22d
sachin798815 <sachin798815@gmail.com>
2025-03-23 13:13:12 +0530

    basic meetup item

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,34 +1,9 @@
-import { useRouter } from 'next/router';
-import { useCallback } from 'react';
-import Image from 'next/image';
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
-
-function MeetupItem({ id, image, title, address }) {
-  const router = useRouter();
-
-  const showDetailsHandler = useCallback(() => {
-    router.push(`/${id}`);
-  }, [id, router]);
-
+function MeetupItem({ id, title, address }) {
   return (
-    <li className={classes.item}>
-      <Card>
-        <article className={classes.article}>
-          <div className={classes.image}>
-            <Image src={image} alt={title} width={400} height={250} layout="responsive" />
-          </div>
-          <div className={classes.content}>
-            <h3>{title}</h3>
-            <address>{address}</address>
-          </div>
-          <div className={classes.actions}>
-            <button onClick={showDetailsHandler} className={classes.button}>
-              Show Details
-            </button>
-          </div>
-        </article>
-      </Card>
+    <li>
+      <h3>{title}</h3>
+      <p>{address}</p>
+      <button>Show Details</button>
     </li>
   );
 }
