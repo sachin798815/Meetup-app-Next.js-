57e4cc2bc2462cce1fba00a89907325fb9608ca8
sachin798815 <sachin798815@gmail.com>
2025-03-25 11:44:11 +0530

    navigation to meetup detail

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,9 +1,17 @@
+import { useRouter } from 'next/router';
+
 function MeetupItem({ id, title, address }) {
+  const router = useRouter();
+
+  const showDetailsHandler = () => {
+    router.push(`/${id}`);
+  };
+
   return (
     <li>
       <h3>{title}</h3>
       <p>{address}</p>
-      <button>Show Details</button>
+      <button onClick={showDetailsHandler}>Show Details</button>
     </li>
   );
 }
