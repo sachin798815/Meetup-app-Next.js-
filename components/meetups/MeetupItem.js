6b1a4d4470e0e9ce38151ee81ea61d79f643b2e8
sachin798815 <sachin798815@gmail.com>
2025-03-27 10:59:13 +0530

    item image integrated

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 function MeetupItem({ id, image, title, address }) {
   const router = useRouter();
 
@@ -10,14 +12,16 @@ function MeetupItem({ id, image, title, address }) {
       <Card>
         <article className={classes.content}>
           <div className={classes.image}>
-            <img src={image} alt={title} />
+            <Image src={image} alt={title} width={400} height={250} layout="responsive" />
           </div>
           <div className={classes.details}>
             <h3>{title}</h3>
             <address>{address}</address>
           </div>
           <div className={classes.actions}>
-            <button onClick={showDetailsHandler}>Show Details</button>
+            <button onClick={showDetailsHandler} className={classes.button} aria-label={`View details of ${title}`}>
+              Show Details
+            </button>
           </div>
         </article>
       </Card>
