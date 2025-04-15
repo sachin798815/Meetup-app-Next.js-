5451fbcdf192ad073bec7a8b8a501f2452cb99a9
sachin798815 <sachin798815@gmail.com>
2025-04-15 12:28:14 +0530

    meetup summary component created

---
+++ components/meetups/MeetupSummary.js
@@ -0,0 +1,10 @@
+function MeetupSummary() {
+  return (
+    <section>
+      <h1>React Meetup Hub</h1>
+      <p>Browse exciting developer meetups and explore opportunities to connect and learn.</p>
+    </section>
+  );
+}
+
+export default MeetupSummary;
