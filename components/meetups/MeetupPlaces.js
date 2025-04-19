9161e7fc5ee5c97ae74a173591a1d8f6bc4c0995
sachin798815 <sachin798815@gmail.com>
2025-04-19 11:05:44 +0530

    places component created

---
+++ components/meetups/MeetupPlaces.js
@@ -0,0 +1,10 @@
+function MeetupPlaces() {
+    return (
+      <section>
+        <h2>Nearby Meetup Places</h2>
+        <p>Loading places...</p>
+      </section>
+    );
+  }
+  
+  export default MeetupPlaces;
