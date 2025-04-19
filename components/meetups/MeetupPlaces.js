01351350f80b1ed34d6940d56c40e91716eecf76
sachin798815 <sachin798815@gmail.com>
2025-04-19 11:09:35 +0530

    places fetched from api

--- components/meetups/MeetupPlaces.js
+++ components/meetups/MeetupPlaces.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
+
 function MeetupPlaces() {
-    return (
-      <section>
-        <h2>Nearby Meetup Places</h2>
-        <p>Loading places...</p>
-      </section>
+const [places, setPlaces] = useState([]);
+    
+useEffect(() => {
+async function fetchPlaces() {
+    try {
+        const res = await fetch('https://api.api-ninjas.com/v1/city?country=India');
+        const data = await res.json();
+        setPlaces(data.slice(0, 5));
+      } catch (error) {
+        console.error('Failed to fetch places:', error);
+      }
+}
+fetchPlaces();
+},[]);
+
+return (
+    <section>
+    <h2>Nearby Meetup Places</h2>
+    <ul>
+        {places.map((place, idx) => (
+            <li key={idx}>{place.name}</li>
+        ))}
+    </ul>
+    </section>
     );
-  }
-  
-  export default MeetupPlaces;
+}
+
+export default MeetupPlaces;
